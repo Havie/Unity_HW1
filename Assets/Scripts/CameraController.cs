04af3e126d7681c828e4648f493d828b5d80353d
StoshFerhobin <sfdatz@live.com>
2020-02-03 17:18:57 -0600

    Moving Character

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+    public float lookSmooth = 0.09f;
+    public Vector3 offsetFromTarget = new Vector3(0, 2, -4);
+    public float xTilt = 15f;
+
+    Vector3 destination = Vector3.zero;
+    CharacterMovement charController;
+    float rotateVelo = 0;
+
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setCameraTarget(target);
+    }
+
+
+    private void LateUpdate()
+    {
+        moveToTarget();
+
+       if(Input.GetMouseButton(1))
+        {
+            LookAtTargetRMB();
+            print("Down");
+        }
+       else
+           lookAtTarget();
+
+
+
+    }
+
+    public void setCameraTarget(Transform t)
+    {
+        target = t;
+        if (target != null)
+        {
+            if (target.GetComponent<CharacterMovement>())
+            {
+                charController = target.GetComponent<CharacterMovement>();
+            }
+            else
+                Debug.LogError("Cameras target missing character controller");
+        }
+        else
+            Debug.LogError("Camera has no target");
+    }
+
+    void moveToTarget()
+    {
+        destination = charController.getTargetRotation() * offsetFromTarget;
+        destination += target.position;
+        transform.position = destination;
+    }
+
+    void lookAtTarget()
+    {
+        //transform.LookAt(target);
+
+        //better lerp?
+        float eulerYAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref rotateVelo, lookSmooth);
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, eulerYAngle, 0);
+    }
+
+    void LookAtTargetRMB()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+
+}
---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public GameObject Character;
+    public float movementSpeed = 5f;
+    public float inputDelay = 0.1f;
+    public float ForwardVelo = 12;
+    public float RotateVelo = 100f;
+
+    public Animator charAnimator;
+
+    Quaternion targetRotation;
+    Rigidbody rBody;
+    float forwardInput,turnInput;
+
+
+    public Vector3 location;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRotation = transform.rotation;
+        rBody = GetComponent<Rigidbody>();
+        if (!rBody)
+            Debug.LogError("No Rigidbody found on Character");
+
+        charAnimator = GetComponent<Animator>();
+        if (!charAnimator)
+            Debug.LogError("No Animator??");
+
+        forwardInput = 0;
+        turnInput = 0;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        Turn();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Run();
+    }
+
+    public Quaternion getTargetRotation()
+    {
+        return targetRotation;
+    }
+
+    void GetInput()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+    }
+
+    void Run()
+    {
+        if(Mathf.Abs(forwardInput) > inputDelay)
+        {
+            //move
+            rBody.velocity = transform.forward * forwardInput * ForwardVelo;
+            charAnimator.SetBool("isMoving", true);
+        }
+        else
+        {
+            //zero velo
+            rBody.velocity = Vector3.zero;
+            charAnimator.SetBool("isMoving", false);
+        }
+    }
+
+    void Turn()
+    {
+        if (Mathf.Abs(turnInput) > inputDelay)
+        {
+            targetRotation *= Quaternion.AngleAxis(RotateVelo * turnInput * Time.deltaTime, Vector3.up);
+        }
+        transform.rotation = targetRotation;
+    }
+}
