e1ef3a80116678b5ce0f9269a2ea3e6b44520c23
Steve Datz <sfdatz@live.com>
2020-03-11 14:14:11 -0500

    DuckStuff

---
+++ Assets/Scripts/LerpLight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpLight : MonoBehaviour
+{
+    //https://www.youtube.com/watch?v=cD-mXwSCvWc
+    private Vector3 newPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPos = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PositionChanging();
+    }
+
+    void PositionChanging()
+    {
+        Vector3 PosA = new Vector3(-5, 4, 0);
+        Vector3 PosB = new Vector3(5, 3, 0);
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            newPos = PosA;
+        }
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            newPos = PosB;
+        }
+
+        transform.position = Vector3.Lerp(this.transform.position, newPos, Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/MoveDuck.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDuck : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _TargetDuck;
+    [SerializeField]
+    Vector3 _VectorToTarget;
+    float _DuckSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _VectorToTarget = (_TargetDuck.transform.position - transform.position);
+       // transform.position = transform.position + _VectorToTarget;
+
+        _VectorToTarget = _VectorToTarget.normalized * _DuckSpeed;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceBetweenDucks = Vector3.Distance(transform.position, _TargetDuck.transform.position);
+        if (distanceBetweenDucks > 1)
+        {
+            _VectorToTarget = _VectorToTarget.normalized * _DuckSpeed;
+            transform.LookAt(_TargetDuck.transform.position);
+            // transform.position = transform.position + _VectorToTarget;
+
+            //transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime);
+
+        }
+
+
+        /** If dot product is 1, vectors coincide
+        * If dot product is 0, vectors at right angle
+        * If dot product is -1, vectors in opposite direction
+        * Take 〖𝑐𝑜𝑠〗^(−1)(V · W) to get exact angle
+         */
+    }
+}
