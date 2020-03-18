cae8a05276aac8b87431b32228193ef2abe341ee
Havie <sfdatz@live.com>
2020-03-18 08:22:00 -0500

    PowerPoint

---
+++ Assets/Scripts/PlatformMover.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMover : MonoBehaviour
+{
+
+    public Transform start;
+    public Transform end;
+    float speed = 0.3f;
+    float elapsedTime = 0;
+    public float weight;
+    private float tripTime=3;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //CNTRL+K + CNTRL +C / U  to comment uncomment
+        //WILD Trig Code to do a lerp....?! How does it work
+        //weight = Mathf.Cos(elapsedTime * speed * 2 * Mathf.PI) * 0.5f + 0.5f;
+        //transform.position = start.position * weight + end.position * (1 - weight);
+        //elapsedTime += Time.deltaTime;
+
+        if (transform.position == end.position)
+            swap();
+
+        transform.position = Vector3.Lerp(start.position, end.position, speed * elapsedTime % tripTime);
+        elapsedTime += Time.deltaTime;
+    }
+
+    public void swap()
+    {
+        Transform tmp = start;
+        start = end;
+        end = tmp;
+        elapsedTime = 0;
+    }
+}
---
+++ Assets/Scripts/PlayerCollisions.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Physics.Raycast(this.transform.position, transform.forward, out hit, 5 );
+        if(hit.collider)
+        {
+            if(hit.collider.gameObject.tag.Equals("PickUp"))
+            {
+                Debug.Log("Found:" + hit.collider.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("Platform"))
+        {
+            transform.parent = other.transform;
+            transform.parent.GetComponent<PlatformMover>().enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("Platform"))
+        {
+            transform.parent = other.transform;
+            transform.parent.GetComponent<PlatformMover>().enabled = false;
+        }
+    }
+}
