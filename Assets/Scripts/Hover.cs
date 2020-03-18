def9118a34ec051fdfad24607709541be0785f0a
Havie <sfdatz@live.com>
2020-03-18 08:58:15 -0500

    Slide3

---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    private float HoverForce = 25f;
+
+    private void OnTriggerStay(Collider other)
+    {
+       // Debug.Log("I AM" + this.gameObject + "   collider is:" + other.gameObject);
+        other.GetComponent<Rigidbody>().AddForce(Vector3.up * HoverForce, ForceMode.Acceleration);
+    }
+}
